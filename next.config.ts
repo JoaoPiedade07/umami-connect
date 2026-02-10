ad90fe0dc12a9b75cf5111f9cd904f5e16bd29a5
João Piedade <153988325+JoaoPiedade07@users.noreply.github.com>
2026-02-10 13:17:46 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // Especifique explicitamente a raiz do projeto
+    root: "/Users/joaopiedade/Desktop/Projects/Startup/umami-connect"
+  }
 };
 
 export default nextConfig;
